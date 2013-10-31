1e1c1366b137bc356bf128a56d4ed5cb93450547
Becky Hogan <becky_hogan@taylor.edu>
2013-10-31 01:27:02 +0000

    in class work with adding databases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
   
+  has_many :players
+  has_many :contests
+  has_many :referees
+  
   validates :username, presence: true, length: {maximum: 25}, uniqueness: true
   validates :email, presence: true, format: /\A[^\s]+@([a-z0-9]+\.)+\w+\z/i
 
